46e649f4f7ed9a112d0a671c4bdee09a000b4aca
iam-bolla <160156727+iam-bolla@users.noreply.github.com>
2025-09-01 02:20:21 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,4 +6,4 @@ docker pull sravyabolla/simple-python-flask-app:latest
 
 
 # Run the Docker image as a container
-docker run -it -p 5000:5000 sravyabolla/simple-python-flask-app:latest
+docker run -d -p 5000:5000 sravyabolla/simple-python-flask-app
