fef81d8f7bb7e25a02c02a1e8903c110b8cbddb3
iam-bolla <160156727+iam-bolla@users.noreply.github.com>
2025-09-02 15:06:51 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 set -e
 
-CONTAINER_NAME=flask-app
-IMAGE_NAME=sravyabolla/simple-python-flask-app:latest
-HOST_PORT=5000
-CONTAINER_PORT=5000
-
-echo "Checking for existing container..."
-if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
-    echo "Stopping old container..."
-    docker stop $CONTAINER_NAME || true
-    echo "Removing old container..."
-    docker rm $CONTAINER_NAME || true
-    # Small wait to make sure port is released
-    sleep 3
-fi
-
-echo "Pulling latest image..."
-docker pull $IMAGE_NAME
-
-echo "Starting new container..."
-docker run -d --name $CONTAINER_NAME -p $HOST_PORT:$CONTAINER_PORT $IMAGE_NAME
+containerid=$(docker ps | awk 'NR==2 {print $1}')
+docker rm -f "$containerid"
 
 
 
